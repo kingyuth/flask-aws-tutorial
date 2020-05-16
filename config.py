143b451b597cb055396b0e5c86a9966642998f5e
Narin Yuth <narin.fasade@gmail.com>
2020-05-16 16:43:30 +1000

    update config

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 # The other elements are the same as used in the tutorial
 # format: (user):(password)@(db_identifier).amazonaws.com:3306/(db_name)
 
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://flaskdemo:flaskdemo@flaskdemo.cwsaehb7ywmi.us-east-1.rds.amazonaws.com:3306/flaskdemo'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://admin:muliya092@database-1.cemxwn0ljgmn.ap-southeast-2.rds.amazonaws.com:3306/kinge'
 
 # Uncomment the line below if you want to work with a local DB
 #SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
@@ -10,4 +10,4 @@ SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://flaskdemo:flaskdemo@flaskdemo.cwsaehb
 SQLALCHEMY_POOL_RECYCLE = 3600
 
 WTF_CSRF_ENABLED = True
-SECRET_KEY = 'dsaf0897sfdg45sfdgfdsaqzdf98sdf0a'
+SECRET_KEY = 'mEsLUU6CUBSCZvc5HQtU4kfaItBoeLau4tq3YT8z'
